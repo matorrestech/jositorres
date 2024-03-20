fef84f6dc903cfe1ff3547ad154f2e3396ca6142
Matheus <mt@matheustorres.com>
2024-03-20 16:16:58 +0000

    Update fonts and styles in CSS files - 20.3.24

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,10 +1,29 @@
 import React from "react"
 import "./Main.css"
 
+import florDeLis from "../../assets/flordelis.png"
+
 export default function Main() {
     return (
         <>
-            <div className="main">TESTE</div>
+            <div className="main">
+                <div className="florDeLis">
+                    <img src={florDeLis} alt="" />
+                </div>
+                <div className="texto">
+                    <p className="ola">Olá, Eu sou</p>
+
+                    <p className="josiTorres">Josi Torres</p>
+
+                    <p className="nailDesigner">Nail Designer</p>
+                </div>
+
+                <div className="button">
+                    <button className="btnAgendamento">
+                        Agende seu Horário
+                    </button>
+                </div>
+            </div>
         </>
     )
 }
