3acc858955d54239b827c481750fb3f65b2f5a69
Matheus <mt@matheustorres.com>
2024-03-22 21:41:53 +0000

    Add Swiper component for carousel functionality - 22.03.24

--- src/Components/Jobs/Jobs.jsx
+++ src/Components/Jobs/Jobs.jsx
@@ -1,6 +1,16 @@
 import React from "react"
 import "./Jobs.css"
 
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules"
+
+import { Swiper, SwiperSlide } from "swiper/react"
+
+// Import Swiper styles
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+import "swiper/css/scrollbar"
+
 import Tab from "react-bootstrap/Tab"
 import Tabs from "react-bootstrap/Tabs"
 
@@ -15,7 +25,23 @@ export default function Jobs() {
                     justify
                 >
                     <Tab eventKey="all" title="Todos">
-                        Todos
+                        <Swiper
+                            // install Swiper modules
+                            modules={[Navigation, Pagination, Scrollbar, A11y]}
+                            spaceBetween={50}
+                            slidesPerView={3}
+                            navigation
+                            pagination={{ clickable: true }}
+                            scrollbar={{ draggable: true }}
+                            onSwiper={(swiper) => console.log(swiper)}
+                            onSlideChange={() => console.log("slide change")}
+                        >
+                            <SwiperSlide>Slide 1</SwiperSlide>
+                            <SwiperSlide>Slide 2</SwiperSlide>
+                            <SwiperSlide>Slide 3</SwiperSlide>
+                            <SwiperSlide>Slide 4</SwiperSlide>
+                            ...
+                        </Swiper>
                     </Tab>
                     <Tab eventKey="artistic" title="Artístico">
                         Artístico
