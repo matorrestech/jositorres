7e7073f9eb8b83cf62db594ef0f3102411e302eb
Matheus <mt@matheustorres.com>
2024-03-22 19:14:27 +0000

    Add custom tabs and styling to Jobs component - 22.03.24

--- src/Components/Jobs/Jobs.jsx
+++ src/Components/Jobs/Jobs.jsx
@@ -1,5 +1,33 @@
 import React from "react"
+import "./Jobs.css"
+
+import Tab from "react-bootstrap/Tab"
+import Tabs from "react-bootstrap/Tabs"
 
 export default function Jobs() {
-    return <div>Jobs</div>
+    return (
+        <>
+            <div>
+                <Tabs
+                    defaultActiveKey="all"
+                    id="justify-tab-example"
+                    className="d-flex justify-content-center custom-tabs"
+                    justify
+                >
+                    <Tab eventKey="all" title="Todos">
+                        Todos
+                    </Tab>
+                    <Tab eventKey="artistic" title="Artístico">
+                        Artístico
+                    </Tab>
+                    <Tab eventKey="polish" title="Gel">
+                        Gel
+                    </Tab>
+                    <Tab eventKey="extension" title="Alongamento">
+                        Alongamento
+                    </Tab>
+                </Tabs>
+            </div>
+        </>
+    )
 }
